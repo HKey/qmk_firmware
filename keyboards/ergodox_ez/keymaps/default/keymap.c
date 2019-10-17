5f8f14591c5c875f4bbd2800b8c90b68408c21a0
HKey <s06139@gmail.com>
2019-10-17 09:59:19 +0900

    Use one shot keys for control and shift

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [NMAL] = LAYOUT_ergodox(  // layer 0 : default
   // left hand
-  KC_ZKHK,      KC_1,    KC_2,     KC_3,    KC_4,     KC_5,     KC_NO,
-  KC_TAB,       KC_Q,    KC_W,     KC_E,    KC_R,     KC_T,     KC_DEL,
-  KC_LCTRL,     KC_A,    KC_S,     KC_D,    KC_F,     KC_G,
-  KC_LSFT,      KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,     KC_ENT,
-  KC_NO,        KC_NO,   KC_LWIN,  KC_LALT, KC_MHEN,
+  KC_ZKHK,       KC_1,    KC_2,     KC_3,    KC_4,     KC_5,     KC_NO,
+  KC_TAB,        KC_Q,    KC_W,     KC_E,    KC_R,     KC_T,     KC_DEL,
+  OSM(MOD_LCTL), KC_A,    KC_S,     KC_D,    KC_F,     KC_G,
+  OSM(MOD_LSFT), KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,     KC_ENT,
+  KC_NO,         KC_NO,   KC_LWIN,  KC_LALT, KC_MHEN,
                                                       TG(SYMB), TG(ARRW),
                                                                 KC_NO,
                                             KC_SPC,   MO(SYMB), MO(ARRW),
